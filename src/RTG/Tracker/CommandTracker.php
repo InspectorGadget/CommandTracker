e942e3c64ab8eb821198d40f4f6ad76112bfe816
InspectorGadget <igadget28@gmail.com>
2017-01-08 22:17:20 +0800

    Initial commit

---
+++ src/RTG/Tracker/CommandTracker.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace RTG\Tracker;
+
+use pocketmine\utils\Config;
+use pocketmine\plugin\PluginBase;
+use pocketmine\Server;
+use pocketmine\Player;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerCommandPreprocessEvent;
+
+class CommandTracker extends PluginBase implements Listener {
+	
+	public function onEnable() {
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		$this->logs = new Config($this->getDataFolder() . "logs.txt");
+	}
+	
+	public function onFilter(PlayerCommandPreprocessEvent $e) {
+		$msg = $e->getMessage();
+		$carray = implode(" ",trim($msg));
+		$m = $carray[0];
+		$p = $e->getPlayer();
+		$n = $p->getName();
+		$r = intval(time());
+		$time = date("m-d-Y H:i:s", $r);
+			
+			if($p->isOp() or $p->hasPermission("system.track")) {
+				if($m === "/kick" or $m === "/ban" or $m === "/pardon") {
+					$this->logs->set($time, "Player: " . $n . " | Command: " . $msg);
+					$this->logs->save();
+				}
+			}
+	}
+	
+	public function onDisable() {
+	}
+	
+}
