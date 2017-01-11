745f56da20ec238adb7b2727e92e6956291a9764
InspectorGadget <igadget28@gmail.com>
2017-01-11 17:56:41 +0800

    Released Final Version... || Tested fully :)

--- src/RTG/Tracker/CommandTracker.php
+++ src/RTG/Tracker/CommandTracker.php
@@ -1,5 +1,10 @@
 <?php
 
+/*
+ * All rights reserved RTGNetworkkk
+ * Please give credits :)
+*/
+
 namespace RTG\Tracker;
 
 use pocketmine\utils\Config;
@@ -8,13 +13,15 @@ use pocketmine\Server;
 use pocketmine\Player;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerCommandPreprocessEvent;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
 
 class CommandTracker extends PluginBase implements Listener {
 	
 	public function onEnable() {
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		@mkdir($this->getDataFolder());
 		@mkdir($this->getDataFolder() . "players");
-		// $this->logs = new Config($this->getDataFolder() . "player/".$n.".yml", Config::YAML, array()); #incase
 	}
 	
 	public function onFilter(PlayerCommandPreprocessEvent $e) {
@@ -27,15 +34,27 @@ class CommandTracker extends PluginBase implements Listener {
 		$time = date("m-d-Y H:i:s", $r);
 			
 			if($p->isOp() or $p->hasPermission("system.track")) {
-				if($m === "/kick" or $m === "/ban" or $m === "/pardon") {
-					$this->logs = new Config($this->getDataFolder() . "players/".$n.".yml", Config::YAML, array());
+				if($m === "/kick" or $m === "/ban" or $m === "/pardon" or $m === "/me") {
+					$this->logs = new Config($this->getDataFolder() . "players/" . strtolower($p->getName()) . ".yml", Config::YAML);
 					$this->logs->set($time, "Player: " . $n . " | Command: " . $msg);
 					$this->logs->save();
 				}
 			}
 	}
 	
+	public function onCommand(CommandSender $sender, Command $cmd, $label, array $param) {
+		switch(strtolower($cmd->getName())) {
+			
+			case "check":
+				$sender->sendMessage("CommandTracker is running perfectly!");
+				return true;
+			break;
+			
+		}
+	}		
+	
 	public function onDisable() {
+		$this->logs->save();
 	}
 	
 }
